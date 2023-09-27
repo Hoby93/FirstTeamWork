9f48a214b9c95104545a2190b7db1d66d07708ad
Stirio273 <tsioryitokiana273@gmail.com>
2023-09-27 16:22:39 +0300

    Ticket1

---
+++ FrontServlet.java
@@ -0,0 +1,24 @@
+package bonjour;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+public class FrontServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher dispa = request.getRequestDispatcher("WEB-INF/jsp/hello.jsp");
+        dispa.forward(request, response);
+    }
+}
